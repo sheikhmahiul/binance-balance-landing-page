66e6a8664921433aaf6258a67ecb555c93203fbe
Mahiul <mahiul@example.com>
2026-08-30 20:09:36 +0600

    Switch vercel.json to builds format and add display_errors

--- api/index.php
+++ api/index.php
@@ -1,7 +1,11 @@
 <?php
 
+ini_set('display_errors', '1');
+error_reporting(E_ALL);
+
 // Forward Vercel serverless requests to Laravel's public/index.php
 require __DIR__ . '/../public/index.php';
+
 
 
 
